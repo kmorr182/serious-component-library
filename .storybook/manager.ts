59bb6c540650b923b8fb4a4d2a587a7068fc9fa9
kmorr182 <kmorr182@gmail.com>
2026-09-04 17:05:45 -0500

    Fix missed renames.

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -4,7 +4,7 @@ import { create } from 'storybook/theming/create';
 const theme = create({
   base: 'light',
   brandTitle: 'serious-component-library',
-  brandUrl: 'https://github.com/kmorr182/component-library',
+  brandUrl: 'https://github.com/kmorr182/serious-component-library',
   brandImage: 'component-library-storybook.svg',
   brandTarget: '_self',
 });
